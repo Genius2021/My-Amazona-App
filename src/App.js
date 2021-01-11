23b2a618b1518570c05f388f61f339a562660e85
Emmanuel Onwuka <emmanuelonwukaazu@gmail.com>
2021-01-11 17:29:06 +0100

    product details page creation
    I basically used react-router-dom to create each product page and styled it using css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import data from './data'
-import { FaStar, FaStarHalf } from "react-icons/fa";
+import { BrowserRouter, Route } from 'react-router-dom';
+import ProductScreen from './components/screens/ProductScreen';
+import HomeScreen from './components/screens/HomeScreen';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
     <header className="row">
         <div>
@@ -15,33 +17,12 @@ function App() {
         </div>
     </header>
     <main>
-        <div className="row center">
-          {
-            data.products.map(product => (
-              <div key={ product._id } className="card">
-                <a href={`/product/${ product._id }`}>
-                    <img className="medium" src={ product.image } alt={ product.name } />
-                </a>
-                <div className="card-body">
-                    <a href={`/product/${ product._id }`}>
-                        <h2>{ product.name }</h2>
-                    </a>
-                    <div className="rating">
-                        <span><FaStar /></span>
-                        <span><FaStar /></span>
-                        <span><FaStar /></span>
-                        <span><FaStar /></span>
-                        <span><FaStarHalf /></span>
-                    </div>
-                    <div className="price">${ product.price }</div>
-                </div>
-              </div>
-             ) )
-          }
-        </div>
+      <Route path="/product/:id" component={ProductScreen}></Route>
+      <Route path="/" component={HomeScreen} exact></Route>
     </main>
     <footer className="row center">All rights reserved</footer>
 </div>
+</BrowserRouter>
   );
 }
 
